dfa288f277165dd611ea07de0557d88102069927
devplayg <devplayg@gmail.com>
2020-05-26 09:51:33 +0900

    added slice example

--- baekjoon/1260/main.go
+++ baekjoon/1260/main.go
@@ -5,6 +5,9 @@ import (
 	"sort"
 )
 
+var queue []*node
+
+// Using map
 func main() {
 	// Input
 	var nodeCount, lineCount, startId int
@@ -19,13 +22,36 @@ func main() {
 		myNodeMap.m[line1].link(myNodeMap.m[line2])
 	}
 
-	myNodeMap.dfs(startId)
+	dfs(myNodeMap.m[startId])
 
 	fmt.Println()
 	myNodeMap.clear()
 
-	myNodeMap.bfs(startId)
+	myNodeMap.bfs(myNodeMap.m[startId])
 }
+
+// Using slice
+//func main() {
+//	// Input
+//	var nodeCount, lineCount, startId int
+//	fmt.Scanf("%d %d %d", &nodeCount, &lineCount, &startId)
+//
+//	// Create nodes and draw lines
+//	nl := newNodeList(nodeCount)
+//
+//	var line1, line2 int
+//	for i := 0; i < lineCount; i++ {
+//		fmt.Scanf("%d %d", &line1, &line2)
+//		nl.list[line1-1].link(nl.list[line2-1])
+//	}
+//
+//	dfs(nl.list[startId-1])
+//
+//	fmt.Println()
+//	nl.clear()
+//
+//	nl.bfs(nl.list[startId-1])
+//}
 
 // node
 type node struct {
@@ -46,9 +72,8 @@ func (n *node) visit() {
 
 // nodeMap
 type nodeMap struct {
-	m     map[int]*node
-	queue []*node
-	idx   int
+	m   map[int]*node
+	idx int
 }
 
 func newNodeMap(count int) *nodeMap {
@@ -60,49 +85,107 @@ func newNodeMap(count int) *nodeMap {
 		}
 		m[n] = node
 	}
-	return &nodeMap{m: m, queue: make([]*node, 0)}
+	return &nodeMap{m: m}
 }
 
 func (nm *nodeMap) clear() {
 	for k, _ := range nm.m {
 		nm.m[k].visited = false
 	}
+	queue = make([]*node, 0)
 }
 
-func (nm *nodeMap) dfs(id int) {
-	if nm.m[id].visited {
-		return
-	}
-
-	node := nm.m[id]
-	node.visit()
-
-	// Sort
-	sort.Slice(node.children, func(i, j int) bool {
-		return node.children[i].id < node.children[j].id
-	})
-
-	for i := 0; i < len(node.children); i++ {
-		nm.dfs(node.children[i].id)
-	}
-}
-
-func (nm *nodeMap) bfs(id int) {
-	nm.visitByBfs(id)
-	for nm.idx < len(nm.queue)-1 || len(nm.queue) == 1 {
-		nm.visitByBfs(nm.queue[nm.idx].id)
+func (nm *nodeMap) bfs(nd *node) {
+	nm.visitByBfs(nd)
+	for nm.idx < len(queue)-1 || len(queue) == 1 {
+		nm.visitByBfs(queue[nm.idx])
 		nm.idx++
 	}
 }
 
-func (nm *nodeMap) visitByBfs(id int) {
-	node := nm.m[id]
-	if node.visited {
+func (nm *nodeMap) visitByBfs(nd *node) {
+	if nd.visited {
 		return
 	}
-	node.visit()
+	nd.visit()
 
-	for i := 0; i < len(node.children); i++ {
-		nm.queue = append(nm.queue, node.children[i])
+	for i := 0; i < len(nd.children); i++ {
+		queue = append(queue, nd.children[i])
+	}
+}
+
+// nodeList
+type nodeList struct {
+	list  []*node
+	queue []*node
+	idx   int
+}
+
+func newNodeList(count int) *nodeList {
+	nl := nodeList{
+		list:  make([]*node, count),
+		queue: make([]*node, 0),
+		idx:   0,
+	}
+
+	for i := 0; i < count; i++ {
+		nl.list[i] = &node{
+			id:       i + 1,
+			children: make([]*node, 0),
+			visited:  false,
+		}
+	}
+
+	return &nl
+}
+
+func (nl *nodeList) clear() {
+	for i := range nl.list {
+		nl.list[i].visited = false
+	}
+	queue = make([]*node, 0)
+}
+
+func (nl *nodeList) bfs(nd *node) {
+	nl.visitByBfs(nd)
+
+	for i := 0; i < len(nd.children); i++ {
+		nl.visitByBfs(nd.children[i])
+	}
+
+	for i := 0; i < len(nl.queue); i++ {
+		nl.visitByBfs(nl.queue[i])
+	}
+}
+
+func (nl *nodeList) visitByBfs(nd *node) {
+	if nd.visited {
+		return
+	}
+
+	nd.visit()
+	for i := range nd.children {
+		nl.queue = append(nl.queue, nd.children[i])
+	}
+}
+
+func dfs(nd *node) {
+	if nd.visited {
+		return
+	}
+
+	nd.visit()
+
+	if len(nd.children) < 1 {
+		return
+	}
+
+	// Sort
+	sort.Slice(nd.children, func(i, j int) bool {
+		return nd.children[i].id < nd.children[j].id
+	})
+
+	for i := 0; i < len(nd.children); i++ {
+		dfs(nd.children[i])
 	}
 }
--- baekjoon/1260/main_test.go
+++ baekjoon/1260/main_test.go
@@ -4,11 +4,10 @@ import (
 	"bytes"
 	"io/ioutil"
 	"os"
-	"strings"
 	"testing"
 )
 
-func TestSearch(t *testing.T) {
+func TestSearchByMap(t *testing.T) {
 	type args struct {
 		nodeCount int
 		lineCount int
@@ -67,23 +66,101 @@ func TestSearch(t *testing.T) {
 				nm.m[line[0]].link(nm.m[line[1]])
 			}
 			startCaputringStdout()
-			nm.dfs(tt.args.startNo)
+			dfs(nm.m[tt.args.startNo])
 			res := stopCaptureStdout()
-			if strings.TrimSpace(res) != tt.args.want[0] {
+			if res != tt.args.want[0] {
 				t.Errorf("(DFS) got = %s, want = %s", res, tt.args.want[0])
 			}
 
 			nm.clear()
 
 			startCaputringStdout()
-			nm.bfs(tt.args.startNo)
+			nm.bfs(nm.m[tt.args.startNo])
 			res = stopCaptureStdout()
-			if strings.TrimSpace(res) != tt.args.want[1] {
+			if res != tt.args.want[1] {
 				t.Errorf("(BFS) got = %s, want = %s", res, tt.args.want[1])
 			}
 		})
 	}
+}
 
+func TestSearchByList(t *testing.T) {
+	type args struct {
+		nodeCount int
+		lineCount int
+		startNo   int
+		lines     [][2]int
+		want      [2]string
+	}
+
+	tests := []struct {
+		name string
+		args args
+	}{
+		{name: "problem-1", args: struct {
+			nodeCount int
+			lineCount int
+			startNo   int
+			lines     [][2]int
+			want      [2]string
+		}{nodeCount: 4, lineCount: 5, startNo: 1, lines: [][2]int{
+			{1, 2},
+			{1, 3},
+			{1, 4},
+			{2, 4},
+			{3, 4},
+		}, want: [2]string{"1 2 4 3", "1 2 3 4"}}},
+
+		{name: "problem-2", args: struct {
+			nodeCount int
+			lineCount int
+			startNo   int
+			lines     [][2]int
+			want      [2]string
+		}{nodeCount: 5, lineCount: 5, startNo: 3, lines: [][2]int{
+			{5, 4},
+			{5, 2},
+			{1, 2},
+			{3, 4},
+			{3, 1},
+		}, want: [2]string{"3 1 2 5 4", "3 1 4 2 5"}}},
+
+		{name: "problem-3", args: struct {
+			nodeCount int
+			lineCount int
+			startNo   int
+			lines     [][2]int
+			want      [2]string
+		}{nodeCount: 1000, lineCount: 1, startNo: 1000, lines: [][2]int{
+			{999, 1000},
+		}, want: [2]string{"1000 999", "1000 999"}}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			nl := newNodeList(tt.args.nodeCount)
+			for _, line := range tt.args.lines {
+				n1Idx := line[0] - 1
+				n2Idx := line[1] - 1
+				nl.list[n1Idx].link(nl.list[n2Idx])
+			}
+			startCaputringStdout()
+			dfs(nl.list[tt.args.startNo-1])
+			res := stopCaptureStdout()
+			if res != tt.args.want[0] {
+				t.Errorf("(DFS) got = %s, want = %s", res, tt.args.want[0])
+			}
+
+			nl.clear()
+
+			startCaputringStdout()
+			nl.bfs(nl.list[tt.args.startNo-1])
+			res = stopCaptureStdout()
+			if res != tt.args.want[1] {
+				t.Errorf("(BFS) got = %s, want = %s", res, tt.args.want[1])
+			}
+		})
+	}
 }
 
 var r, w, oldStdout *os.File
