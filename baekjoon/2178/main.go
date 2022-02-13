6e9e1dac7cd41dfa2bcfe5c3be5b902b174e3947
devplayg <devplayg@gmail>
2022-02-13 20:01:17 +0900

    added

--- baekjoon/2178/main.go
+++ baekjoon/2178/main.go
@@ -1,28 +1,64 @@
 package main
 
+import (
+	"fmt"
+)
+
+type Pos struct {
+	y int
+	x int
+	c int
+}
+
+var N int
+var M int
+var arr = [101][101]int{}
+var visited = [101][101]bool{}
+var queue = make([]*Pos, 0)
+var dy = [4]int{1, -1, 0, 0}
+var dx = [4]int{0, 0, 1, -1}
+
+func valid(y, x int) bool {
+	if !(y > 0 && y <= N && x > 0 && x <= M) {
+		return false
+	}
+	if arr[y][x] == 0 {
+		return false
+	}
+
+	if visited[y][x] {
+		return false
+	}
+	return true
+}
+
 func main() {
-	//scanner := bufio.NewScanner(os.Stdin)
-	//for scanner.Scan() {
-	//	fmt.Println(scanner.Text())
-	//}
-	//
-	//if scanner.Err() != nil {
-	//	// handle error.
-	//}
-	//
-	//fmt.Scan
-	//4 6
-	//101111
-	//101010
-	//101011
-	//111011
-	input := `
-101111
-101010
-101011
-111011
-`
-	m := NewMaze(4, 6, input)
-	m.display()
-	//spew.Dump(m)
+	fmt.Scanf("%d %d", &N, &M)
+	for y := 1; y <= N; y++ {
+		for x := 1; x <= M; x++ {
+			if x == 1 {
+				fmt.Scanf("\n%1d", &arr[y][x])
+				continue
+			}
+			fmt.Scanf("%1d", &arr[y][x])
+		}
+	}
+
+	queue = append(queue, &Pos{1, 1, 1})
+	for len(queue) > 0 {
+		pos := queue[0]
+		queue = queue[1:]
+
+		if pos.y == N && pos.x == M {
+			fmt.Println(pos.c)
+			return
+		}
+		c := pos.c + 1
+		for i := 0; i < 4; i++ {
+			if valid(pos.y+dy[i], pos.x+dx[i]) {
+				queue = append(queue, &Pos{pos.y + dy[i], pos.x + dx[i], c})
+				visited[pos.y+dy[i]][pos.x+dx[i]] = true
+			}
+		}
+	}
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/devplayg/coding-test
+
+go 1.16
+
+require github.com/davecgh/go-spew v1.1.1 // indirect
